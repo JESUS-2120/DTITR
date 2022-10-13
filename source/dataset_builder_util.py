51076ecdd6772aacda80c06aab439c88410e32f4
JESUS-2120 <80478632+JESUS-2120@users.noreply.github.com>
2022-10-13 01:03:09 -0500

    no clusters

--- source/dataset_builder_util.py
+++ source/dataset_builder_util.py
@@ -25,18 +25,13 @@ class dataset_builder():
         dataset = pd.read_csv(self.data_path['data'], sep=',', memory_map=True)
         prot_dictionary = json.load(open(self.data_path['prot_dic']))
         smiles_dictionary = json.load(open(self.data_path['smiles_dic']))
-        clusters = []
         bpe_codes_prot = ''
         bpe_codes_map_prot = ''
         bpe_codes_smiles = ''
         bpe_codes_map_smiles = ''
 
-        for i in self.data_path['clusters']:
-            if 'test' in i:
-                clusters.append(('test', pd.read_csv(i, header=None)))
-            else:
-                clusters.append(('train', pd.read_csv(i, header=None)))
-
+        clusters = [] # this way theres no need to change return()
+        
         if self.data_path['prot_bpe'] != '':
             bpe_codes_prot = codecs.open(self.data_path['prot_bpe'][0])
             bpe_codes_map_prot = pd.read_csv(self.data_path['prot_bpe'][1])
@@ -142,7 +137,5 @@ class dataset_builder():
             smiles_data = tf.convert_to_tensor(self.data_conversion(self.get_data()[0][smiles_column],
                                                                     self.get_data()[2], smiles_max_len).astype('int32'),
                                                dtype=tf.int32)
-
-        kd_values = self.get_data()[0][kd_column].astype('float32')
-
-        return protein_data, smiles_data, kd_values
+            
+        return protein_data, smiles_data
